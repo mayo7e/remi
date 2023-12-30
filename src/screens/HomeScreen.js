eb43c745af503154dd5827c53476c142355960f4
mayo7e <mayowa.wh@gmail.com>
2023-12-30 08:04:29 +0100

    Add Chat Section of Home Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,60 @@
-import {SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
+import {SafeAreaView, ScrollView, StyleSheet, Text, View, Image } from 'react-native'
 import React, { useState } from 'react'
 import Features from '../components/Features';
+// import { ScrollView } from 'react-native-gesture-handler';
+
+import {  dummyMessages } from '../constants';
 
 const PlaceholderImage = require('../../assets/images/bot.png');
 
-const [messages, setMessages] = useState("")
+const [messages, setMessages] = useState(dummyMessages)
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container} >
         <View style={styles.imageContainer}>
-            <Image style={styles.image} source={PlaceholderImage} />
+            <Image  style={styles.imageStyle} source={PlaceholderImage} />
         </View>
         {messages.length > 0 ? (
-            <View>
-                <Text>Assistant</Text>
+            <View style={styles.chatContainer} >
+                <Text style={styles.assistantHeader} >Assistant</Text>
+                <View style={styles.chatBox} >
+
+                <ScrollView>
+                        {messages.map((message, index)=>{
+                           if(message.role === "assistant"){
+                                if(message.content.includes("https")){
+                                    return(
+                                        <View 
+                                        style={styles.imageContainer}
+                                        key={index} >
+                                            <Image 
+                                               source= {{url: message.content}} 
+                                               style={styles.imageStyle}
+                                                resizeMode="contain"
+                                                />
+                                        </View>
+                                        )
+                                }else{
+                                    return(
+                                        <View 
+                                        style={styles.assistantContainer}
+                                        key={index}>
+                                            < Text>{message.content}</Text>
+                                        </View>
+                                    )   
+                                }
+                           }
+                            return(
+                                <View 
+                                style={styles.userContainer}
+                                key={index}>
+                                    < Text>{message.content}</Text>
+                                </View>
+                            )
+                        })}
+                </ScrollView>
+                </View>
             </View>
         ): <Features />}
     </SafeAreaView>
@@ -26,7 +66,8 @@ export default HomeScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // justifyContent: 'center', 
+    
+        // justifyContent: 'flex-end', 
         alignItems: 'center', 
         backgroundColor: 'white',
         gap: '16px'
@@ -39,9 +80,57 @@ const styles = StyleSheet.create({
         paddingTop: '16px'
         
     },
-    image: {
+    imageStyle: {
         width: '100%',
         height: '100%',
         resizeMode: 'contain',
-      }
+      },
+
+      assistantHeader: {
+       fontWeight: 700,
+        fontSize: 24,
+      },
+      
+      chatContainer: {
+         flex: 1,
+          width: "280px",
+          height: '80%',
+         
+        },
+  
+      chatBox: {
+         flex: 1,
+          width: "100%",
+         height: "50%",
+         padding: "16px",
+          borderRadius: 10,
+          backgroundColor: "#CCCCCC"
+        },
+
+
+
+      assistantContainer: {
+        flex: 1,
+          backgroundColor: "#ffffff",
+          padding: "8px",
+          borderRadius: "8px",
+          marginBottom: "16px",
+          width: "60%",
+          
+        },
+      userContainer: {
+          backgroundColor: "#90EE90",
+          alignSelf: "flex-end",
+          padding: "8px",
+          borderRadius: "8px",
+          marginBottom: "16px",
+          width: "60%",
+        },
+        imageContainer: {
+            width: "200px",
+            height: "200px",
+        },
+
+        
+  
 })
